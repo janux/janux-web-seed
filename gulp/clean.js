dfd5620e8602c4a5d95161b674aa797669ee27f5
Philippe Paravicini <pparavicini@janux.org>
2014-03-31 17:29:27 -0700

    change "clean" so that it preserves the dist folder

--- gulp/clean.js
+++ gulp/clean.js
@@ -5,7 +5,7 @@ module.exports = function(gulp, cfg) {
 	
 	gulp.task('clean', function () {
 		return gulp.src([
-			cfg.dir.dist
+			cfg.dir.dist + '/**/*'
 		], {read: false})
 		.pipe(cfg.plugins.clean());
 	});

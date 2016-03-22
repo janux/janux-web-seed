f91dd2d897de68742f72e4b345319d1df8e8a5ab
Philippe Paravicini <pparavicini@janux.org>
2016-03-22 15:53:54 -0700

    remove deprecated gulp-recess

--- gulp/styles.js
+++ gulp/styles.js
@@ -14,20 +14,6 @@ module.exports = function(gulp) {
 			paths: [cfg.dir.bower] // search for imports here
 		};
 
-		var recessOpts = {
-			includePath: [cfg.dir.bower] // search for imports here
-			//
-			// Uncomment these options if you want recess to issue less warnings;
-			// in particular, the strictPropertyOrder is a twitter convention that can
-			// be quite verbose if applied to a stylesheet that was not authored with
-			// this convention
-			//
-			,strictPropertyOrder:  false  // do not complain about properties out of order
-			// ,noOverqualifying:     false // do not complain about overqualifying selectors div#foo.bar
-			// ,zeroUnits:            false // do not complain about adding units to values of 0
-			// ,noUniversalSelectors: false // do not complain about using the universal * selector
-		};
-
 		gulp.src(
 			cfg.fileset.less,
 			{
@@ -35,12 +21,7 @@ module.exports = function(gulp) {
 				base: cfg.dir.src + path.sep
 			})
 			.pipe(gulp.plugins.less(lessOpts).on('error', console.log))
-			.pipe(gulp.plugins.recess(recessOpts).on('error', function(err) {console.log(err)}))
 			.pipe(gulp.plugins.autoprefixer('last 1 version'))
-
-			// name destination file with the 'name' attribute in the package.json file
-			// .pipe(gulp.plugins.concat(cfg.pkg.name + '.css'))
-
 			.pipe(gulp.dest(cfg.dir.dist))
 			.pipe(gulp.plugins.size());
 	});
